8b58ae166362298c29a919961a82527ae14c23b0
Erghi <erghiimannurichsan@gmail.com>
2021-12-10 22:44:04 +0700

    update

---
+++ README.md
@@ -0,0 +1,3 @@
+LOGIN
+- username: admin
+- password: admin
--- protected/views/dashboard/index.php
+++ protected/views/dashboard/index.php
@@ -19,15 +19,10 @@
 <section class="content">
 
     <!-- solid sales graph -->
-    <div class="card bg-gradient-info">
-        <div class="card-header border-0">
-            <h3 class="card-title">
-                <i class="fas fa-walking mr-1"></i>
-                Jumlah Kunjungan
-            </h3>
-        </div>
+    <div class="card">
         <div class="card-body">
-            <canvas class="chart" id="line-chart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+            <p>Created By: Erghi Imannur Ichsan</p>
+            <p>Database: MySQL</p>
         </div>
     </div>
     <!-- /.card -->
